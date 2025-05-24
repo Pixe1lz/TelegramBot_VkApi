d01c532a030d6c30ea6c8adcf336bfaff0e3cd07
Pixe1lz <104935174+Pixe1lz@users.noreply.github.com>
2025-05-24 14:08:23 +0700

    secretTokens

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace First_TelegramBot
     {
         static async Task Main(string[] args)
         {
-            var token_bot = new TelegramBotClient("7626928336:AAF508V9IVJJ9ZrYxM9oIkF7c4ghqjna_Ns");
+            var token_bot = new TelegramBotClient("###");
 
             var MetBot = new BotEngine(token_bot);
 
